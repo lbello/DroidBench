7f3c2c1aa382c4154e8139f4df4e8cee880f2155
Luciano <lbello@us.ibm.com>
2015-07-14 15:18:00 -0400

    description and check point

--- eclipse-project/EmulatorDetection/Network1/src/de/ecspride/Network1.java
+++ eclipse-project/EmulatorDetection/Network1/src/de/ecspride/Network1.java
@@ -11,8 +11,9 @@ import android.text.format.Formatter;
 import android.util.Log;
 /**
  * @testcase_name Network1
- * Practically the same from netH.patch
- * Rage Against the Virtual Machine: Hindering Dynamic Analysis of Android Malware. Thanasis Petsas et al.
+ * Copied from http://stackoverflow.com/a/13007325
+ * The practical attack: 
+ *    - maybe? ./319_DroidKungFu4-ca3c18fa9e7d402d57a3f2138b1d68754de65c86/b.java:619
  */
 public class Network1 extends Activity {
 	
@@ -27,11 +28,21 @@ public class Network1 extends Activity {
 		String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
         Log.d("IBM", "IP: "+ip);
         */
+		String wlanMAC = Utils.getMACAddress("wlan0");
+		String eth0MAC = Utils.getMACAddress("eth0");
+		String IPv4    = Utils.getIPAddress(true);
+		String IPv6    = Utils.getIPAddress(false);
         
-        Log.d("IBM","->"+Utils.getMACAddress("wlan0"));
-        Log.d("IBM","->"+Utils.getMACAddress("eth0") );
-        Log.d("IBM","->"+Utils.getIPAddress(true)    ); // IPv4
-        Log.d("IBM","->"+Utils.getIPAddress(false    )); // IPv6 
+        Log.d("IBM","wlanMAC: "+wlanMAC);
+        Log.d("IBM","eth0MAC: "+eth0MAC);
+        Log.d("IBM","IPv4: "   +IPv4   ); // IPv4
+        Log.d("IBM","IPv6: "   +IPv6   ); // IPv6 
+        
+        if (IPv4.equals("10.0.2.15")) {
+        	System.exit(0);
+        	return;
+        }
+        
         String imei = ((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE)).getDeviceId(); //source
 
     	SmsManager sms = SmsManager.getDefault();
